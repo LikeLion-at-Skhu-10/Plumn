7b169504ec0cf167093b1016a1b10448e70ad97a
Jung, Jaewan <wodhkszxc@gmail.com>
2022-07-20 16:06:29 +0900

    accounts 앱 admin.py 변경

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User
 
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'user_id',
+        'user_pw',
+        'user_name',
+        'user_phone',        
+    )
