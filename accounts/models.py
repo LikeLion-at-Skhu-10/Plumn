6380a8da9a5bda1c9e2b295c714a28d693fb9410
Jung, Jaewan <wodhkszxc@gmail.com>
2022-07-22 17:46:26 +0900

    authenticaiton 테이블 수정

--- accounts/models.py
+++ accounts/models.py
@@ -5,7 +5,7 @@ from django.core.validators import RegexValidator #전화번호 입력을 위한
 # Create your models here.
 class User(models.Model):
     user_id = models.CharField(max_length=32, unique=True, verbose_name='유저 아이디')
-    user_pwd = models.CharField(max_length=128, verbose_name='유저 비밀번호')
+    user_pw = models.CharField(max_length=128, verbose_name='유저 비밀번호')
     user_name = models.CharField(max_length=32, unique=True, verbose_name='유저 이름')
     phoneNumRegex = RegexValidator(regex = r'^01([0|1|6|7|8|9]?)-?([0-9]{3,4})-?([0-9]{4})$')
     user_phone = models.CharField(validators = [phoneNumRegex], max_length=11, unique=True, verbose_name='유저 전화번호')
@@ -27,12 +27,3 @@ class Authentication(models.Model):
     class Meta:
         db_table = 'authenticaiton' 
         verbose_name_plural = "휴대폰 인증 관리 페이지" # admin 페이지에서 나타나는 설명
-    
-    def save(self, *args, **kwargs):
-        self.auth_number = randint(1000, 10000) #4자리 랜덤 난수 생성
-        super().save(*args, **kwargs)
-        self.send_sms() # SMS 전송하기(인증번호 포함)
-        
-    #인증번호 전송
-    def send_sms(self):
-        timestamp = str
