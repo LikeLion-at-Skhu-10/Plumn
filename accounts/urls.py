d1dcb08bec1f17c3f3ea7ee1b4d837a945773c3b
Jung, Jaewan <wodhkszxc@gmail.com>
2022-07-22 17:44:25 +0900

    urls 추가(임시)

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from accounts import views
 
 urlpatterns=[
-    
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('logout/', views.logout, name='logout'),
+    #path('findid/', views.findid, name='findid'),
+    #path('findpw/', views.findpw, name='findpw'),
 ]
