a447038f83ce7c4c496b7ed7aa52ed1f1e7cf040
Jung, Jaewan <wodhkszxc@gmail.com>
2022-07-26 14:46:16 +0900

    views.py 대폭 수정

--- accounts/views.py
+++ accounts/views.py
@@ -1,110 +1,127 @@
-import json, time, random, requests
+import json, requests, time, random
 
-from django.shortcuts import render, redirect
-from django.contrib import auth
+from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib import auth, messages
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import PasswordChangeForm
 from argon2 import PasswordHasher
-from accounts.forms import RegisterForm
 from django.views import View
 from django.http import JsonResponse
-from .models import Authentication
-from .utils import make_signature, access_key, secret_key, send_phone_number, uri, API_URL
+from .models import User#, Authentication
+from django.template import RequestContext
+from django.urls import reverse_lazy
+
+# 이메일 가입을 위한 SMTP 관련 인증
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
+from django.core.mail import EmailMessage
+from django.utils.encoding import force_bytes, force_str
+from .tokens import account_activation_token
 
 # Create your views here.
 def login(request):
     if request.method == 'POST':
-        userid = request.POST['username']
-        pw = request.POST['password']
-        user = auth.authenticate(request, username=userid, password=pw)
+        email = request.POST['email']
+        password = request.POST['password']
+        user = auth.authenticate(request, email=email, password=password)
         
-        if user is not None:
-            auth.login(request, user)
-            return redirect('main')
+        if User.objects.filter(email=email).exists():
+            if user is not None:
+                login(request, user)
+                return redirect('main.html')
+            else:
+                messages.info(request, "비밀번호를 다시 입력해주세요.")
+                return render(request, 'login.html')
         else:
-            return render(request, 'bad_login.html')
+            messages.info(request, "비밀번호가 틀렸습니다.")
+            return render(request, 'login.html')
     else:
         return render(request, 'login.html')
 
-def register(request):
-    register_form = RegisterForm()
-    context = {'forms' : register_form}
-    
-    if request.method == 'POST':
-        register_form = RegisterForm(request.POST)
-        if register_form.is_valid():
-            user = User(
-                user_id = register_form.user_id,
-                user_pw = register_form.user_pw,
-                user_name = register_form.user_name,
-                user_phone = register_form.user_phone
-            )
-            user.save()
-            return redirect('login/')
-        else:
-            context['forms'] = register_form
-            if register_form.erors:
-                for value in register_form.errors.values():
-                    context['error'] = value
-        return render(request, 'accounts/register.html', context)
-    
-    else:
-        return render(request, 'accounts/register.html', context)
 
 def logout(request):
     auth.logout(request)
     return redirect('main')
 
-# 네이버 SMS 인증
-class SmsSendView(View):
-    def send_sms(self, phone_number, auth_number):
-        timestamp = str(int(time.time() * 1000))  
-        headers = {
-            'Content-Type': "application/json; charset=UTF-8", # 네이버 참고서 차용
-            'x-ncp-apigw-timestamp': timestamp, # 네이버 API 서버와 5분이상 시간차이 발생시 오류
-            'x-ncp-iam-access-key': access_key, #(예시 = 'nCH86JcJ6FCl40eYc4qp')
-            'x-ncp-apigw-signature-v2': make_signature(timestamp) # utils.py 이용
-        }
-        body = {
-            "type": "SMS", 
-            "contentType": "COMM",
-            "from": send_phone_number, # 사전에 등록해놓은 발신용 번호 입력, 타 번호 입력시 오류
-            "content": f"[인증번호:{auth_number}]", # 메세지 형식
-            "messages": [{"to": f"{phone_number}"}] # 네이버 양식에 따른 messages.to 입력
-        }
-        body = json.dumps(body)
-        requests.post(API_URL, headers=headers, data=body)
-        
-    def post(self, request):
-        data = json.loads(request.body)
+def register(request):
+    if request.method == 'POST':
+        if request.POST['password'] == request.POST['password_confirm']:
+            email = request.POST.get('email') 
+            #request.POST['']로 html에서 input 값 name을 각각 변수로 지정
+            password = request.POST['password']
+            password_confirm = request.POST['password_confirm']
+            username = request.POST.get('user_name', False)
+            userphone = request.POST['user_phone']
+                    
+            user = User(email=email, password=password, username=username, user_phone=userphone)
+
+            user.save()
+            user.is_active = False # 유저 비활성화
+            #user.save()
+            current_site = get_current_site(request) 
+            message = render_to_string('activation_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            mail_title = "계정 활성화 확인 이메일"
+            mail_to = request.POST['email']
+            email = EmailMessage(mail_title, message, to=[mail_to])
+            email.send()
+            return redirect("login")
+        else:
+            messages.info(request, '비밀번호가 일치하지 않습니다.')
+            return render(request, 'register.html')
+    return render(request, 'register.html')
+            
+# 계정 활성화 함수(토큰을 통해 인증)
+def activate(request, uidb64, token):
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExsit):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        auth.login(request, user)
+        return redirect("login")
+    else:
+        return render(request, 'login.html', {'error' : '계정 활성화 오류'})
+         
+         
+#아이디 찾기
+def findid(request):
+    if request.method == 'POST' :
+        find_id = request.POST['user_phone']
+        user = User.objects.filter(user_phone=find_id)
+        if user is not None:
+            return render(request, 'find_id_success.html', {'user':user}) 
+        else:
+            messages.info(request, "해당 전화번호는 없습니다.")
+            return render(request, 'find_id.html')
+    else:
+        return render(request, 'find_id.html')      
+
+def findidsuccess(request, email):
+    userid = get_object_or_404(User, id=id)
+    return render(request, 'find_id_success.html')
+
+#비밀번호 찾기
+def findpw(request): 
+    if request.method == 'POST':
+        userid = request.POST.get('user_id')
+        userphone = request.POST.get('user_phone')
         try:
-            input_mobile_num = data['phone_number']
-            auth_num = random.randint(1000, 10000) # 랜덤숫자 생성, 4자리로 계획하였다.
-            auth_mobile = Authentication.objects.get(phone_number=input_mobile_num)
-            auth_mobile.auth_number = auth_num
-            auth_mobile.save()
-            self.send_sms(phone_number=data['phone_number'], auth_number=auth_num)
-            return JsonResponse({'message': '인증번호 발송완료'}, status=200)
-        except Authentication.DoesNotExist: # 인증요청번호 미 존재 시 DB 입력 로직 작성
-            Authentication.objects.create(
-                phone_number=input_mobile_num,
-                auth_number=auth_num,
-            ).save()
-            self.send_sms(phone_number=input_mobile_num, auth_number=auth_num)
-            return JsonResponse({'message': '인증번호 발송 및 DB 입력완료'}, status=200)
-
-# 네이버 SMS 인증번호 검증
-class SMSVerificationView(View):
-    def post(self, request):
-        data = json.loads(request.body)
-
-        try:
-            verification = Authentication.objects.get(phone_number=data['phone_number'])
-
-            if verification.auth_number == data['auth_number']:
-                return JsonResponse({'message': '인증 완료되었습니다.'}, status=200)
-
-            else:
-                return JsonResponse({'message': '인증 실패입니다.'}, status=400)
-
-        except Authentication.DoesNotExist:
-            return JsonResponse({'message': '해당 휴대폰 번호가 존재하지 않습니다.'}, status=400)
+            user = User.objects.filter(user_id=userid, user_phone=userphone)
+            if user is not None:
+                return render(request, 'find_pw_success.html', {'user':user})
+        except:
+            if userid is None:
+                return messages.info(request, "해당 아이디가 없습니다.")
+            else :
+                return messages.info(request, "해당 전화번호는 없습니다.")
+    else:
+        return render(request, 'find_pw.html')
