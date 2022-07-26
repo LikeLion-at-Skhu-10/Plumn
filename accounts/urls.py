401f1c02314ad34d4c510f474ca918bfaf3ee746
Jung, Jaewan <wodhkszxc@gmail.com>
2022-07-26 14:45:58 +0900

    urls.py 비밀번호 초기화 url 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
 from accounts import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns=[
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
+
     path('logout/', views.logout, name='logout'),
-    #path('findid/', views.findid, name='findid'),
-    #path('findpw/', views.findpw, name='findpw'),
+    path('findid/', views.findid, name='find_id'),
+    path('findidsuccess/', views.findidsuccess, name='find_id_success'),
+    path('findpw/', views.findpw, name='find_pw'),
+        
+    path('activate/<str:uidb64>/<str:token>/', views.activate, name="activate"), 
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name="password_reset"), #비밀번호 초기화
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
