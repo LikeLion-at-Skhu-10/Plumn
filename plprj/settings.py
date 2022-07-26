ab4928e632adf169bf53bb0649bafb23a8a2ccce
Jung, Jaewan <wodhkszxc@gmail.com>
2022-07-26 14:53:17 +0900

    email인증을 위한 코드 추가

--- plprj/settings.py
+++ plprj/settings.py
@@ -147,3 +147,42 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email 전송
+# 메일을 호스트하는 서버
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+
+# gmail과의 통신하는 포트
+EMAIL_PORT = 587
+
+# 발신할 이메일
+# EMAIL_HOST_USER = '구글아이디@gmail.com'
+EMAIL_HOST_USER = get_secret("EMAIL_HOST_USER")
+
+# 발신할 메일의 비밀번호
+# EMAIL_HOST_PASSWORD = '구글비밀번호'
+EMAIL_HOST_PASSWORD = get_secret("EMAIL_HOST_PASSWORD")
+
+# TLS 보안 방법
+EMAIL_USE_TLS = True
+
+# 사이트와 관련한 자동응답을 받을 이메일 주소
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+#이메일 인증 설정 담당
+ACCOUNT_EMAIL_VERIFICATION = "mandatory"
+
+# 유저가 받은 링크를 클릭하면 회원가입 완료되게끔
+ACCOUNT_CONFIRM_EMAIL_ON_GET = True 
+
+# 사이트와 관련한 자동응답 받을 이메일주소?
+EMAIL_CONFIRMATION_AUTHENTICATED_REDIRECT_URL = '/'
+
+ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = 1
+
+#이메일에 자동으로 표시되는 사이트 정보
+ACCOUNT_EMAIL_SUBJECT_PREFIX = "Plumn"
+
+AUTH_USER_MODEL = 'accounts.User' # 앱label.모델명
