2d419c7409855d27412d4c133f61faecff75c522
Jung, Jaewan <wodhkszxc@gmail.com>
2022-07-28 18:40:34 +0900

    비밀번호 초기화 views 추가

--- accounts/views.py
+++ accounts/views.py
@@ -3,14 +3,17 @@ import json, requests, time, random
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib import auth, messages
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import PasswordChangeForm
-from argon2 import PasswordHasher
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.forms import PasswordResetForm, PasswordContextMixin, PasswordChangeForm
+from django.views.generic import TemplateView, FormView
+#from argon2 import PasswordHasher
 from django.views import View
 from django.http import JsonResponse
 from .models import User#, Authentication
 from django.template import RequestContext
 from django.urls import reverse_lazy
-
+from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView
 # 이메일 가입을 위한 SMTP 관련 인증
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
@@ -24,17 +27,17 @@ def login(request):
     if request.method == 'POST':
         email = request.POST['email']
         password = request.POST['password']
-        user = auth.authenticate(request, email=email, password=password)
+        user = authenticate(request, email=email, password=password)
         
         if User.objects.filter(email=email).exists():
             if user is not None:
-                login(request, user)
-                return redirect('main.html')
+                auth.login(request, user)
+                return redirect('find_id')
             else:
                 messages.info(request, "비밀번호를 다시 입력해주세요.")
                 return render(request, 'login.html')
         else:
-            messages.info(request, "비밀번호가 틀렸습니다.")
+            messages.info(request, "해당 아이디는 존재하지 않습니다.")
             return render(request, 'login.html')
     else:
         return render(request, 'login.html')
@@ -44,6 +47,8 @@ def logout(request):
     auth.logout(request)
     return redirect('main')
 
+#회원가입
+#에러 메세지(이메일, 닉네임, 전화번호)만 추가하면 됨
 def register(request):
     if request.method == 'POST':
         if request.POST['password'] == request.POST['password_confirm']:
@@ -54,7 +59,7 @@ def register(request):
             username = request.POST.get('user_name', False)
             userphone = request.POST['user_phone']
                     
-            user = User(email=email, password=password, username=username, user_phone=userphone)
+            user = User.objects.create_user(email=email, password=password, username=username, user_phone=userphone)
 
             user.save()
             user.is_active = False # 유저 비활성화
@@ -104,3 +109,31 @@ def findid(request):
             return render(request, 'find_id.html')
     else:
         return render(request, 'find_id.html')      
+
+#비밀번호 초기화하기 위해 아이디(=이메일) 존재하는 여부 검색
+class PasswordResetView(PasswordResetView):
+    template_name = "find_pw.html" #템플릿 변경하려면 다음과 같은 형식으로 입력
+    success_url = reverse_lazy('password_reset_done')
+    form_class = PasswordResetForm
+    
+    def form_valid(self, form):
+        if User.objects.filter(email=self.request.POST.get("email")).exist():
+            return super().form_valid(form)
+        else:
+            messages.info("해당하는 이메일이 존재하지 않습니다.")
+            return render(self.request, 'find_pw.html')
+
+#초기화하려는 아이디계정 이메일 존재하면, 초기화 이메일 성공 템플릿
+class PasswordResetDoneView(PasswordContextMixin, TemplateView):
+    template_name = ""
+    title = ("Password reset sent")
+    
+#비밀번호 초기화
+class PasswordResetConfirmView(PasswordContextMixin, FormView):
+    template_name = "find_pw_success.html"
+    success_url = reverse_lazy("password_reset_complete")
+    
+#초기화 성공    
+class PasswordResetCompleteView(PasswordContextMixin, TemplateView):
+    template_name = ""
+    title = ("Password reset complete")
