b7965183a9d9779e3f5fa83b3b98d70ec77cc3d8
zoeyourlife <201814112@office.skhu.ac.kr>
2022-08-09 00:35:50 +0900

    중간 푸쉬

---
+++ static/js/list_btn.js
@@ -0,0 +1,17 @@
+function myFunction() {
+  var x = document.querySelector(".list_btn_wrap_list");
+  if (x.style.display == "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+// $(document).ready(function () {
+//   $("#hide").click(function () {
+//     $("p").hide();
+//   });
+//   $("#show").click(function () {
+//     $("p").show();
+//   });
+// });
--- static/js/title.js
+++ static/js/title.js
@@ -1,8 +1,10 @@
-function length_check() {
-    var desc = $("#story").val();
-  
-    if( desc.length > 30 ) {
-        alert("제목은 30자를 초과할 수 없습니다.");
-        $("#story").val(desc.substring(0, 30));
+$(document).ready(function () {
+  $("#feedback").on("keyup", function () {
+    $("#feedback_cnt").html("(" + $(this).val().length + " / 1000)");
+
+    if ($(this).val().length > 1000) {
+      $(this).val($(this).val().substring(0, 1000));
+      $("#feedback_cnt").html("(1000 / 1000)");
     }
-  }
+  });
+});
