f1bc4f992c23a02babbc168bfbb7f4a68e71e449
김은혜 <lemonherb0323@naver.com>
2022-08-16 00:09:14 +0900

    libr1 complete (exclude json)

--- static/js/followmodal.js
+++ static/js/followmodal.js
@@ -14,8 +14,8 @@ span.onclick = function() {
     modal.style.display = "none";
 }
 
-window.onclick = function(event) {
+window.addEventListener("click", function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
-}
+});
