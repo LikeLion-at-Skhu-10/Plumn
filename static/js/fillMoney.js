229982dd1f167afbc60f98ca651245a737bf9cd1
zoeyourlife <201814112@office.skhu.ac.kr>
2022-08-16 11:55:39 +0900

    pay부분

---
+++ static/js/fillMoney.js
@@ -0,0 +1,58 @@
+function r1View(value) {
+  if (value == "1") {
+    const ptag = document.querySelector(".plumnr1");
+    const ptaga = document.querySelector(".plumnr2");
+    const ptagb = document.querySelector(".plumnr3");
+    const ptagc = document.querySelector(".plumnr4");
+    const ptagd = document.querySelector(".plumnr5");
+    ptag.style.border = "1px solid #fc9595";
+    ptaga.style.border = "1px solid lightgray";
+    ptagb.style.border = "1px solid lightgray";
+    ptagc.style.border = "1px solid lightgray";
+    ptagd.style.border = "1px solid lightgray";
+  } else if (value == "2") {
+    const ptag = document.querySelector(".plumnr2");
+    const ptaga = document.querySelector(".plumnr1");
+    const ptagb = document.querySelector(".plumnr3");
+    const ptagc = document.querySelector(".plumnr4");
+    const ptagd = document.querySelector(".plumnr5");
+    ptag.style.border = "1px solid #fc9595";
+    ptaga.style.border = "1px solid lightgray";
+    ptagb.style.border = "1px solid lightgray";
+    ptagc.style.border = "1px solid lightgray";
+    ptagd.style.border = "1px solid lightgray";
+  } else if (value == "3") {
+    const ptag = document.querySelector(".plumnr3");
+    const ptaga = document.querySelector(".plumnr1");
+    const ptagb = document.querySelector(".plumnr2");
+    const ptagc = document.querySelector(".plumnr4");
+    const ptagd = document.querySelector(".plumnr5");
+    ptag.style.border = "1px solid #fc9595";
+    ptaga.style.border = "1px solid lightgray";
+    ptagb.style.border = "1px solid lightgray";
+    ptagc.style.border = "1px solid lightgray";
+    ptagd.style.border = "1px solid lightgray";
+  } else if (value == "4") {
+    const ptag = document.querySelector(".plumnr4");
+    const ptaga = document.querySelector(".plumnr1");
+    const ptagb = document.querySelector(".plumnr2");
+    const ptagc = document.querySelector(".plumnr3");
+    const ptagd = document.querySelector(".plumnr5");
+    ptag.style.border = "1px solid #fc9595";
+    ptaga.style.border = "1px solid lightgray";
+    ptagb.style.border = "1px solid lightgray";
+    ptagc.style.border = "1px solid lightgray";
+    ptagd.style.border = "1px solid lightgray";
+  } else if (value == "5") {
+    const ptag = document.querySelector(".plumnr5");
+    const ptaga = document.querySelector(".plumnr1");
+    const ptagb = document.querySelector(".plumnr2");
+    const ptagc = document.querySelector(".plumnr3");
+    const ptagd = document.querySelector(".plumnr4");
+    ptag.style.border = "1px solid #fc9595";
+    ptaga.style.border = "1px solid lightgray";
+    ptagb.style.border = "1px solid lightgray";
+    ptagc.style.border = "1px solid lightgray";
+    ptagd.style.border = "1px solid lightgray";
+  }
+}
