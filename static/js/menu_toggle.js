d507fd90e49e498d726002ad7d38866959e237c0
GraceKim527 <lemonherb0323@naver.com>
2022-11-19 22:07:09 +0900

    ADD: notification

--- static/js/menu_toggle.js
+++ static/js/menu_toggle.js
@@ -1,6 +1,9 @@
 var menu = document.getElementById("menu__container");
 var toggle = document.getElementById("menu__box");
 var checkbox = document.getElementById("checkbox");
+var button = document.getElementById("notification_button");
+var notif = document.getElementsByClassName("notif_container")[0];
+
 
 function menutoggle(){
     document.getElementById("menu__box").classList.toggle("menu");
@@ -15,5 +18,13 @@ window.onclick = function(event) {
         menu.style.display = "none";
         toggle.className = "";
         checkbox.checked = false;
+    }
+}
+
+function notification() {
+    button.classList.toggle("toggle");
+    notif.style.display = "inline";
+    if (button.className == ""){
+        notif.style.display = "none";
     }
 }
---
+++ static/js/notification.js
@@ -0,0 +1,2 @@
+var button = document.getElementById("#notification_button");
+var notification = document.getElementsByClassName(".notif_container");
